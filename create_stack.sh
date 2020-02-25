ff915c559432be69a513700f6c766ffb6aa6c920
Felix Geelhaar <felix.geelhaar@sixt.com>
2020-02-25 15:39:58 +0100

    Refactor: Adding and adjust the necessary ports and userdata

--- create_stack.sh
+++ create_stack.sh
@@ -2,4 +2,5 @@ aws cloudformation create-stack \
 --stack-name $1 \
 --region us-west-2 \
 --template-body file://$2 \
---parameters file://$3
+--parameters file://$3 \
+--capabilities CAPABILITY_IAM
--- update_stack.sh
+++ update_stack.sh
@@ -2,4 +2,5 @@ aws cloudformation update-stack \
 --stack-name $1 \
 --region us-west-2 \
 --template-body file://$2 \
---parameters file://$3
+--parameters file://$3 \
+--capabilities CAPABILITY_IAM
